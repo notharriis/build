a1919ab6fc37c600f4d83f7b3cd7dcc1df6be270
notharriis <78409775+notharriis@users.noreply.github.com>
2022-02-21 12:33:46 +0400

    Add files via upload

---
+++ build/precache-manifest.323d38e66677bc2cb0e187599283870e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8935cde3aa1337adc3776f5f062ac931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4317832205af0208727",
+    "url": "/static/css/main.a8ee93d8.chunk.css"
+  },
+  {
+    "revision": "bf784e4b3a1c954304b4",
+    "url": "/static/js/2.0993b177.chunk.js"
+  },
+  {
+    "revision": "9141e6603b2ba65e9f944cb50ce12050",
+    "url": "/static/js/2.0993b177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f28545187bd37ab2a4",
+    "url": "/static/js/3.3249f589.chunk.js"
+  },
+  {
+    "revision": "b4317832205af0208727",
+    "url": "/static/js/main.cc1705d8.chunk.js"
+  },
+  {
+    "revision": "99a2e816f1a67884dab7",
+    "url": "/static/js/runtime-main.dfac70ce.js"
+  },
+  {
+    "revision": "ac5888f000b8ed6b0976b7678b56dbfe",
+    "url": "/static/media/BreatheFireIii.ac5888f0.ttf"
+  }
+]);
